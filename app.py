7b2fb55070301b02f5bb116f7607c45a10bc1381
Ruth <144852850+Ruth-Jensen@users.noreply.github.com>
2024-06-07 14:14:01 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,177 @@
+# cd recipes
+# .\venv\Scripts\Activate
+# python app.py
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+cookbook = []
+class Recipe:
+    def __init__(self, title, ingredients, instructions, tags):
+        self.Title = title
+        self.Ingredients = ingredients
+        self.Instructions = instructions
+        self.Tags = tags
+
+def add_to_cookbook(recipe):
+    cookbook.append(recipe)
+
+def remove_from_cookbook(recipe):
+    cookbook.remove(recipe)
+
+def update_cookbook(recipe, new_recipe):
+    cookbook.remove(recipe)
+    cookbook.append(new_recipe)
+
+def save_cookbook():
+    with open('data.txt', 'w') as file:
+        file.write("Family Cookbook")
+        for recipe in cookbook:
+            file.write(f"\n{recipe.Title}\n{recipe.Ingredients}\n{recipe.Instructions}\n{recipe.Tags}")
+
+def load_cookbook():
+    with open('data.txt', 'r') as file:
+        lines = file.readlines()
+        lines = file.readlines()[1:]
+        for i in range(0, len(lines), 4):
+            if i + 3 < len(lines):
+                recipe = Recipe(lines[i].strip(), lines[i+1].strip(), lines[i+2].strip(), lines[i+3].strip())
+                cookbook.append(recipe)
+
+
+
+
+
+
+def sort_by_name():
+    with open('data.txt', 'r') as file:
+        lines = file.readlines()
+    
+    recipes = []
+    for i in range(0, len(lines), 4):
+        if i + 3 < len(lines):
+            recipe = [lines[i].strip(), lines[i+1].strip(), lines[i+2].strip(), lines[i+3].strip()]
+            recipes.append(recipe)
+    
+    recipes.sort(key=lambda x: x[0].upper())
+    
+    with open('data.txt', 'w') as file:
+        for i, recipe in enumerate(recipes):
+            file.write('\n'.join(recipe))
+            if i < len(recipes) - 1:
+                file.write('\n')
+
+
+
+
+@app.route('/api/read-file', methods=['GET'])
+def read_file():
+    try:
+        with open('data.txt', 'r') as file:
+            lines = file.readlines()
+        
+        # Group lines into chunks of 4
+        grouped_lines = [lines[i:i + 4] for i in range(0, len(lines), 4)]
+        
+        # Remove newline characters and strip lines
+        grouped_lines = [[line.strip() for line in group] for group in grouped_lines]
+        
+        return jsonify({"data": grouped_lines})
+    except FileNotFoundError:
+        return jsonify({"error": "File not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/write-file', methods=['POST'])
+def write_file():
+    try:
+        contentTitle = request.json.get('content_title')
+        contentIngredients = request.json.get('content_ingredients')
+        contentInstructions = request.json.get('content_instructions')
+        contenttags = request.json.get('content_tags')
+        print('\n')
+        print(contentTitle + contentIngredients + contentInstructions + contenttags)
+        with open('data.txt', 'a+') as file:
+            file.write("\n" + contentTitle)
+            file.write("\n" + contentIngredients)
+            file.write("\n" + contentInstructions)
+            file.write("\n" + contenttags)
+            
+        sort_by_name()
+        return jsonify({"message": "Recipe Added successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/delete-file', methods=['POST'])
+def delete_file():
+    try:
+        index = request.json.get('content')
+        index = index * 4
+        with open('data.txt', 'r') as file:
+            lines = file.readlines()
+
+        del lines[index:index + 4]
+        if lines and lines[-1].endswith('\n'):
+            lines[-1] = lines[-1].rstrip('\n')
+
+        with open('data.txt', 'w') as file:
+            file.writelines(lines)
+        return jsonify({"message": "Recipe deleted successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/update-file', methods=['POST'])
+def update_file():
+    try:
+        index = request.json.get('index')
+        index = index * 4
+        contentTitle = request.json.get('content_title')
+        contentIngredients = request.json.get('content_ingredients')
+        contentInstructions = request.json.get('content_instructions')
+        contenttags = request.json.get('content_tags')
+
+        with open('data.txt', 'r') as file:
+            lines = file.readlines()
+
+        lines[index] = contentTitle + '\n'
+        lines[index + 1] = contentIngredients + '\n'
+        lines[index + 2] = contentInstructions + '\n'
+        lines[index + 3] = contenttags + '\n'
+
+        with open('data.txt', 'w') as file:
+            file.writelines(lines)
+
+        sort_by_name()
+        return jsonify({"message": "Recipe updated successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+# Berry Filled Sweet Bread
+# Yeast 2 1/4 tsp, Sugar 1 cup and 1 tbsp, Warm Water 2 cups, Salt 1 tsp, flour 5-6 cups and 2 tbsp, Frozen Berries 1-2 cups, Lemon Juice 1 tbsp, Cornstarch Slurry 1 tbsp, Cream Cheese 8oz, Whipped Cream 1/4 cup, Powdered Sugar 1-2 cup, 1 Egg
+# Dough: Gently stir 2 1/4 tsp yeast, 1 tbsp sugar, and 1/4 cup of warm water in a large bowl. Cover the bowl with a dish towel and let it rise a little, approximately 10-15 minutes. Then add an additional 1/2 cup of sugar, 1 3/4 cup of warm water, and 1 tsp salt. Add in 5 cups of flour. Knead the dough while adding in up to 1 more cup of flour, just until the dough has the right consistency. Place the dough in a lightly greased bowl somewhere warm. Cover with a dish towel and let rise for approximately 1 hour. - Berry Compote: Put 1-2 cups of frozen berries in a pot and 1/2 cup of sugar, more or less depending on the taste, 1 tbsp lemon juice, and 1 tbsp cornstarch. For this part of the recipe, you can adjust the amount of all the ingredients to your liking. Just remember, if you decide to add more cornstarch after the compote has heated up, you must add it in a slurry. Mix the cornstarch with a little water until the cornstarch is fully combined, then pour it into the compote. - Cream Cheese Filling: Microwave the cream cheese for about 30 seconds, or until it's soft. Add 1/4 cup whipped cream, 1-2 cups powdered sugar, just add poudered sugar until you like the taste, and finally add 1 tbsp flour. Stir until fully combined. - Combine and Bake: Once your dough has risen, preheat your oven to 350�F. Take the dough and roll it out on a floured surface. mentally divide the dough into three equal parts lengthwise. Cut the two outer sections into strips, going horizontally, such that the strips are still connected to the main part of the dough. Cover the middle portion of the dough with the cream cheese filling. Make a little well in the filling, going lengthwise. Fill the well with the berry compote. Take the strips on the sides of the dough, fold them over the top, and braid. place on a baking sheet, get a bowl and crack one egg in it and beat the egg, paint the top of the loaf with the egg, sprinkle with sugar, and bake for about 30 minutes, or until golden brown. - Serve: Once the bread is ready, remove the bread from the oven and let it cool down. Serve and enjoy!
+# /images/berry-filled-sweet-bread.jpg
+# Berry Scones
+# Flour 2 cups, Sugar 1/3 cup, Baking Powder 2 tsp, Salt 1/2 tsp, Butter 1/2 cup, Frozen Berries, 1 Lemon Zest, Heavy Cream 3/4 cup, 1 egg
+# Combine sugar and lemon zest together and smoosh it between your fingers until it feels like wet sand. Combine dry ingredients in bowl including lemon sugar. - In another bowl combine heavy cream and egg. - Cut cold butter into 1 cm cubes. Add butter to dry ingredients and squish butter cubes into the mixture the way you would for biscuits or a rough pie dough. Add frozen fruit to dry mixture and mix. Add wet mixture to dry until fully combined. - Roll into a circular shape, approx. 1/2 an inch tall and freeze. - Cut into triangles, brush with heavy cream and sprinkle with sugar and bake at 350 for about 20 min or until golden brown. After they cool, dust with powdered sugar.
+# none
+# Creamy Potato Soup
+# Butter 1/2 cup, 1/2 Onion, flour 1/2 cup, Water or Broth 2-3 cups, milk 1 cup, pepper 1/2 tsp, garlic powder 1/2 tsp, thyme 1 tsp, salt 1 tsp, 1 bag frozen peas and carrots, 2 cubed potatoes, 2 chicken breasts
+# Cube and boil potatoes till fork tender but not super soft or when you mix it, you will have mashed potatoes in your soup. Set them aside. - Boil the chicken, shred it, set it aside. - Cut up your onion and cook it with the butter in a pot until onion is thoroughly cooked. Add flour and mix continually (your making a roux, don't let it burn). Add small amounts of milk at a time and mix so it stays thick, do the same for water / broth. Add all your seasonings into the pot and mix. Add frozen veggies and cook until they aren't frozen anymore. Add potatoes and chicken and heat up. Once it's all warm it's finished.
+# none
+# Egg roll in a bowl
+# 1 pound lean ground beef, 1 tbsp olive oil, 1/2 tsp salt, 1/4 tsp pepper, 1/2 onion finely diced, 1 carrot julienned or coarsely grated, 3 cloves of minced garlic, 3 cups cabbage thinly sliced, 1 tsp ground ginger, 1/4 cup low sodium soy sauce, 2 tsp sesame oil, 1/2 tsp sugar, 1 tbsp chopped green onion optional, 1/4 tsp sesame seeds optional
+# Set a large skillet over medium/high heat and add oil. Once hot, add ground beef and brown until no longer pink, about 5 minutes, breaking up the meat with a spatula as it's cooked. Season with salt and pepper. - Add onion and carrots and saute until onion is tender, 5-7 minutes, stirring occasionally. Add the garlic and cook another 30 seconds, stirring constantly. - Finally add the cabbage, ginger, soy sauce, sesame oil, and sugar. Continue sauteeing for 5-7 minutes, stirring occasionally, or until cabbage is tender.
+# /images/egg-roll-in-a-bowl.jpg
+# Fried Rice
+# Day Old Rice, Soy Sauce, Brown Sugar, Sesame Oil, Frozen Veggies, Chicken, Eggs, Garlic Powder, Ginger, Salt, Pepper
+# Cook your eggs with salt pepper and garlic powder and set it aside. Cook your chicken with whatever seasonings you want into bite sized cubes and set it aside. - Add rice to pan with oil and cook on high heat breaking apart the rice. After rice is broken apart add soy sauce, sesame oil, brown sugar, garlic powder, ginger, pepper, and salt all to taste. Then ass veggies and cook until veggies are done stirring occasionally. Then add eggs and chicken back into it and cook just until eggs and chicken are warmed up
+# none
+# Taco Soup
+# Beef, Bell Pepers 1-2, Onion 1, Oil / Butter 1 tbsp, Diced Garlic 1 tbsp optional, Beef Broth or Water, Tomato Chunks 1-2 cans, Black Beans 1-2 cans, Corn 1 can, Chicken Bullion 2-4 tbsp, Cumin 2-4 tbsp, Garlic Powder 1-2 tbsp, Red Pepper Flakes 1 tsp, Black Pepper 1-2 tsp, Salt to taste, Cilantro optional, Sour Cream optional, Tortilla Chips optional, Fresh Diced Tomatos optional, Diced Avocato optional
+# Cook the ground beef without seasonings and set aside. Dice the onions and bell peppers. Put the onions, bell peppers, and diced garlic in a pot with some oil or butter and saute until soft. Add all the canned food to the pot, and then add your beef broth until it is the right thickness. Add all the seasonings. Make sure to taste as you go; add more or less of whatever you like. Cook until boiling; add the beef you set aside to the pot, and then it's ready to sever. Optionally, you can add cilantro, sour cream, tortilla chips, Fresh Diced Tomatos, and Diced Avocato as toppings.
+# /images/taco-soup.jpg
---
+++ client/src/App.js
@@ -0,0 +1,294 @@
+//cd recipes\client
+//npm start
+
+
+// ---------------------------------------------------------------------IMPORTS-----------------------------------------------------------------------------
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+
+  // -------------------------------------------------------------------STATE VARIABLES-----------------------------------------------------------------
+  // All recipes in the file
+  const [fileContent, setFileContent] = useState([]);
+  // A list of the newly created recipe info
+  const [newRecipe, setNewRecipe] = useState({
+    title: '',
+    ingredients: '',
+    instructions: '',
+    tags: ''
+  });
+  // A list of the edited recipe info
+  const [editedRecipe, setEditedRecipe] = useState({
+    title: '',
+    ingredients: '',
+    instructions: '',
+    tags: ''
+  });
+  // The index of the currently displayed recipe
+  const [currentRecipe, setCurrentRecipe] = useState(0);
+  // Whether or not to show the add recipe, edit recipe, or delete recipe forms
+  const [showAddRecipe, setShowAddRecipe] = useState(false);
+  const [showEditRecipe, setShowEditRecipe] = useState(false);
+  const [showDeleteRecipe, setShowDeleteRecipe] = useState(false);
+  // The name of the recipe the user entered to delete
+  const [deleteRecipeName, setDeleteRecipeName] = useState('');
+
+
+
+  // ----------------------------------------------------------FUNCTIONS TO FETCH FILE CONTENT------------------------------------------------------------------
+  // Fetch file content on component mount
+  useEffect(() => {
+    fetchFileContent();
+  }, []);
+
+  const fetchFileContent = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/read-file');
+      setFileContent(response.data.data);
+      if (currentRecipe >= response.data.data.length) {
+        setCurrentRecipe(response.data.data.length - 1);
+      }
+    } catch (error) {
+      console.error('Error reading file:', error.response ? error.response.data : error.message);
+    }
+  };
+
+
+
+  // ----------------------------------------------------------FUNCTIONS TO MAKE CHANGES THE FILE---------------------------------------------------------
+  // Add the new recipe to the file
+  const addRecipe = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/api/write-file', { 
+      content_title: newRecipe.title,
+      content_ingredients: newRecipe.ingredients,
+      content_instructions: newRecipe.instructions,
+      content_tags: newRecipe.tags });
+      alert('Recipe added successfully');
+      fetchFileContent();
+      setNewRecipe({
+        title: '',
+        ingredients: '',
+        instructions: '',
+        tags: ''
+      });
+      showAddRecipeForm(false);
+    } catch (error) {
+      console.error('Error writing to file:', error.response ? error.response.data : error.message);
+    }
+  };
+
+
+
+  // edit the selected recipe in the file
+  const editRecipe = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post('http://localhost:5000/api/update-file', {
+        index: currentRecipe,
+        content_title: editedRecipe.title,
+        content_ingredients: editedRecipe.ingredients,
+        content_instructions: editedRecipe.instructions,
+        content_tags: editedRecipe.tags
+      });
+      alert('Recipe edited successfully');
+      fetchFileContent();
+      setEditedRecipe({
+        title: '',
+        ingredients: '',
+        instructions: '',
+        tags: ''
+      });
+      showEditRecipeForm(false);
+    } catch (error) {
+      console.error('Error editing file:', error.response ? error.response.data : error.message);
+    }
+  };
+
+
+
+  // delete seclected recipe from the file
+  const deleteRecipe = async (e) => {
+    e.preventDefault();
+    setDeleteRecipeName('');
+    if (fileContent[currentRecipe][0] === deleteRecipeName) {
+      try {
+        await axios.post('http://localhost:5000/api/delete-file', { content: currentRecipe });
+        alert('Recipe deleted successfully');
+        showDeleteRecipeForm(false);
+        fetchFileContent();
+      } catch (error) {
+        console.error('Error deleting file:', error.response ? error.response.data : error.message);
+      }
+    } else {
+      alert('Recipe name does not match.');
+    }
+  };
+
+
+
+  // ---------------------------------------------FUNCTIONS TO DISPLAY AND RECEIVE RECIPE DATA TO AND FROM THE USER------------------------------------------
+  
+  // Switch the recipe being displayed
+  const switchDisplayedRecipe = (index) => {
+    setCurrentRecipe(index);
+  };
+
+
+
+  // FUNCTIONS SHOW AND ADD NEW RECIPE DATA
+  // Show the add recipe form
+  const showAddRecipeForm = (isVisible) => {
+    setShowAddRecipe(isVisible);
+    setShowEditRecipe(false);
+    setShowDeleteRecipe(false);
+  };
+
+  // change the new recipe info to match the info the user entered
+  const addNewRecipeData = (e) => {
+    const { name, value } = e.target;
+    setNewRecipe({ ...newRecipe, [name]: value });
+  };
+
+
+
+  // FUNCTIONS SHOW AND ADD EDITED RECIPE DATA
+  // Show the edit recipe form
+  const showEditRecipeForm = (isVisible) => {
+    setShowEditRecipe(isVisible);
+    setShowDeleteRecipe(false);
+    setShowAddRecipe(false);
+  };
+
+  // change the edited recipe info to match the changes the user made
+  const addEditedRecipeData = (e) => {
+    const { name, value } = e.target;
+    setEditedRecipe(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  // Show the edit recipe form and set the edited recipe info to the current recipe info
+  const showFormAndCurrentInfo = (index) => {
+    setCurrentRecipe(index);
+    showEditRecipeForm(true);
+    setEditedRecipe({
+      title: fileContent[index][0],
+      ingredients: fileContent[index][1],
+      instructions: fileContent[index][2],
+      tags: fileContent[index][3]
+    });
+  };
+  
+
+
+  // FUNCTIONS SHOW AND ADD DELETE RECIPE DATA
+  // Show the delete recipe form
+  const showDeleteRecipeForm = (isVisible) => {
+    setShowDeleteRecipe(isVisible);
+    setShowEditRecipe(false);
+    setShowAddRecipe(false);
+  };
+
+  // gets the name of the recipe the user entered to delete
+  const handleDeleteInputChange = (e) => {
+    setDeleteRecipeName(e.target.value);
+  };
+
+
+  // ----------------------------------------------------------------RENDERING THE APP-------------------------------------------------------------
+  return (
+    <div className="App">
+
+      <div className="cookbook">
+        <h1>Recipes</h1>
+        {fileContent.map((group, index) => (
+          <button key={index} onClick={() => switchDisplayedRecipe(index)}>{group[0]}</button>
+        ))}
+      </div>
+
+      <div className='cookbook_buttons'>
+        <button onClick={() => showAddRecipeForm(true)}>Add Recipe</button>
+        {fileContent && fileContent[currentRecipe] && fileContent[currentRecipe][0] && (
+          <button onClick={() => showDeleteRecipeForm(true)}>Delete {fileContent[currentRecipe][0]}</button>
+        )}
+        {fileContent && fileContent[currentRecipe] && fileContent[currentRecipe][0] && (
+          <button onClick={() => showFormAndCurrentInfo(currentRecipe)}>Edit {fileContent[currentRecipe][0]}</button>
+        )}
+      </div>
+
+      <main>
+        {fileContent.length > 0 && (
+          <div className="recipe">
+            <h1 className='recipe_title'>{fileContent[currentRecipe][0]}</h1>
+            <ul>
+              {fileContent[currentRecipe][1].split(', ').map((ingredient, i) => (
+                <li key={i}>{ingredient.trim()}</li>
+              ))}
+            </ul>
+            <ol>
+              {fileContent[currentRecipe][2].split(' - ').map((instruction, i) => (
+                <li key={i}>{instruction.trim()}</li>
+              ))}
+            </ol>
+            <img src={fileContent[currentRecipe][3]} alt={`tags for ${fileContent[currentRecipe][0]}`} />
+          </div>
+        )}
+
+        <form onSubmit={addRecipe} style={{ display: showAddRecipe ? 'block' : 'none' }}>
+          <div className="new_recipe_form">
+            <h3 className='form_title'>Create New Recipe</h3>
+            <input type="text" name="title" className="title_input" placeholder="Enter Recipe Name" value={newRecipe.title} onChange={addNewRecipeData} required />
+            <textarea type="text" name="ingredients" className="ingredients_input" placeholder="Enter Ingredients separated by commas. example: flour, sugar, salt" value={newRecipe.ingredients} onChange={addNewRecipeData} required />
+            <textarea type="text" name="instructions" className="instructions_input" placeholder="Enter Instructions separated by dashes. example: step 1 - step 2 - step 3" value={newRecipe.instructions} onChange={addNewRecipeData} required />
+            <input type="text" name="tags" className="tags_input" placeholder="Enter tags URL / file path. If no tags, type 'none'" value={newRecipe.tags} onChange={addNewRecipeData} required />
+            <div className="button_group">
+              <button type="submit">Add Recipe</button>
+              <button type="button" onClick={() => showAddRecipeForm(false)}>Cancel</button>
+            </div>
+          </div>
+        </form>
+
+        <form onSubmit={editRecipe} style={{ display: showEditRecipe ? 'block' : 'none' }}>
+          <div className="edit_recipe_form">
+            {fileContent[currentRecipe] && (
+              <>
+                <h3 className='form_title'>Edit {fileContent[currentRecipe][0]}</h3>
+                <input type="text" name="title" className="title_input" placeholder="Enter Recipe Name" value={editedRecipe.title} onChange={addEditedRecipeData} required />
+                <textarea type="text" name="ingredients" className="ingredients_input" placeholder="Enter Ingredients separated by commas. example: flour, sugar, salt" value={editedRecipe.ingredients} onChange={addEditedRecipeData} required />
+                <textarea type="text" name="instructions" className="instructions_input" placeholder="Enter Instructions separated by dashes. example: step 1 - step 2 - step 3" value={editedRecipe.instructions} onChange={addEditedRecipeData} required />
+                <input type="text" name="tags" className="tags_input" placeholder="Enter tags URL / file path. If no tags, type 'none'" value={editedRecipe.tags} onChange={addEditedRecipeData} required />
+              </>
+            )}
+            <div className="button_group">
+              <button type="submit">Edit Recipe</button>
+              <button type="button" onClick={() => showEditRecipeForm(false)}>Cancel</button>
+            </div>
+          </div>
+        </form>
+
+
+        <form onSubmit={deleteRecipe} style={{ display: showDeleteRecipe ? 'block' : 'none' }}>
+          <div className="delete_recipe_form">
+            <h3 className='form_title'>Are you sure you want to delete this recipe?</h3>
+            <input type="text" name="name" className="name_input" placeholder="Enter Recipe Name to confirm. Your input will be case sensitive" value={deleteRecipeName} onChange={handleDeleteInputChange} required />
+            <div className="button_group">
+              <button type="submit">Yes</button>
+              <button type="button" onClick={() => showDeleteRecipeForm(false)}>No</button>
+            </div>
+          </div>
+        </form>
+
+
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
